37ee3dce0befa75c1344230e54b07a5e0fedd61f
amarjeet <amarjeetkapoor1>
2016-09-11 23:10:29 +0530

    updates resoltuion of vector map

--- contour.sh
+++ contour.sh
@@ -4,6 +4,9 @@ $file
 file=$2
 no_lines=$(cat $file| wc -l)
 
+v.in.ascii -z z=3  --overwrite input=$1 output=fieldData
+v.build map=fieldData
+
 echo $no_lines
 
     for i in $(seq 1 $no_lines)
@@ -17,9 +20,7 @@ echo $no_lines
         echo "next points"
         elev_val="DTM$i"
         contour_val="contourVec$i"
-        v.in.ascii -z z=3  --overwrite input=$1 output=fieldData
-        v.build map=fieldData
-        g.region n=$var2 s=$((var2-1500)) e=$var1 w=$((var1-1000))
+        g.region n=$var2 s=$((var2-1500)) e=$var1 w=$((var1-1000)) nsres=1 ewres=0.01
         v.surf.rst --overwrite input=fieldData layer=0 elev=$elev_val 
         r.contour --overwrite input=$elev_val output=$contour_val step=.5 cut=0
     done
