570b21001ba009784bb58069d97e4dc7e31107fe
amarjeet <amarjeetkapoor1>
2016-09-20 10:50:59 +0530

    makes contour.sh more readable

--- README.md
+++ README.md
@@ -4,13 +4,33 @@
 
 cd ../Path/to/TheRoadProject
 
-python TheRoad.py \<inputFile.csv> \<Section> \<distance-between-two-sections-lines>
+###For Generation section points: 
+        This script will give you set of points <i>A<sub>1</sub>, A<sub>2</sub>, A<sub>3</sub> ...... A<sub>n</sub><i> at particular distance *D* on given polyline representing the road. Plus a Set of points *(P<sub>n1</sub>,P<sub>n2</sub>)* perpendicular to a point at *P/2* distance apart from point An
+           
+    * python TheRoad.py \<inputFile.csv> \<Section> \<distance-between-two-sections-lines>
+    * python  TheRoad.py input.csv *P* *D*
+    * e.g. 
+    `python TheRoad.py input.csv 10 40`
+    
+    
+### GIS Scripts:
+    First open GRASS and select/create Location
 
-bash contour.sh \<DATA set for generating map> \<File defining region for each map>
+1. This script will generate contours in Grass and also raster and vector maps
+for given set of data. Data set will be in CSV format `X|Y|Z` (You can refer
+example/GrassData.txt). We can define region of maps in CSV format in another
+CSV file containing only north, east, south, west values for given map. We can
+also divide the full map into small maps to increase the speed of execution or
+for many other purposes by providing more than one north, east, south, west
+values for each smaller map.  (You can refer example/DM1.txt)
+    bash contour.sh \<DATA set for generating map> \<File defining region for each map>
 
 bash road.sh \<points to generate profile > \<output file name>
 
 bash makeSection.sh \<List of section points>
+    
+###To get graph of output of TheRoad.py:
+sage plot.sage \< inputFile.csv>
 
 ##example:
    cd ../Path/to/TheRoadProject/example
@@ -22,8 +42,4 @@ bash makeSection.sh \<List of section points>
    bash ../road.sh Area.csv Final.txt
    
    bash ../makeSection.sh section.csv
-    
-###To get graph
-sage plot.sage \< inputFile.csv>
-
 
--- contour.sh
+++ contour.sh
@@ -1,27 +1,53 @@
 #/bin/sh!
 
-$file
-file=$2
-no_lines=$(cat $file| wc -l)
 
+#This script will generate contours in Grass and also raster and vector maps
+#for given set of data. Data set will be in CSV format `X|Y|Z` (You can refer
+#example/GrassData.txt). We can define region of maps in CSV format in another
+#CSV file containing only north, east, south, west values for given map. We can
+#also divide the full map into small maps to increase the speed of execution or
+#for many other purposes by providing more than one north, east, south, west
+#values for each smaller map.  (You can refer example/DM1.txt)
+
+
+############# Delete all --overwrite If running script for first time ##########
+
+
+#Provide input points and plot them in vector map.
 v.in.ascii -z z=3  --overwrite input=$1 output=fieldData
 v.build map=fieldData
 
+
+#Count numbers of regions to be created
+no_lines=$(cat $f2| wc -l)
+
 echo $no_lines
 
+#create Map for all the regions
     for i in $(seq 1 $no_lines)
     do
-
+        
+        #getting data from input file describing the region. 
         data=$(awk "NR>=$i&&NR<=$i" $file)
         var1=$(echo $data | cut -d',' -f1)
         var2=$(echo $data | cut -d',' -f2)
-
-        echo "x1: " $var1 " y1: " $var2
+        var3=$(echo $data | cut -d',' -f3)
+        var4=$(echo $data | cut -d',' -f4)
+        
         echo "next points"
+        echo "North: " $var1 " South: " $var2  " East: " $var2  " West: " $var2
+        
+        #name of new raster MAP
         elev_val="DTM$i"
+        #name of new vector map
         contour_val="contourVec$i"
-        g.region n=$var2 s=$((var2-1500)) e=$var1 w=$((var1-1000)) nsres=1 ewres=0.01
+        
+        # Create region with resolution in y direction "nsres=1" and x direction "ewres=0.01"
+        g.region n=$var1 s=$var2 e=$var3 w=$var4 nsres=1 ewres=0.01
+        
+        # Create vector map with input as fieldData 
         v.surf.rst --overwrite input=fieldData layer=0 elev=$elev_val 
+        # Create raster map  
         r.contour --overwrite input=$elev_val output=$contour_val step=.5 cut=0
     done
 
